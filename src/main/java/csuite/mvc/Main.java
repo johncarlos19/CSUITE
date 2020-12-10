13b7bb5753a86ee088f9ea2bd60ef3ea6c310c63
John Lopez <johncarlos1943@gmail.com>
2020-12-10 02:37:37 -0400

    agregando cache

--- src/main/java/csuite/mvc/Main.java
+++ src/main/java/csuite/mvc/Main.java
@@ -66,7 +66,7 @@ public class Main {
 
         ApiControlador apiControlador = new ApiControlador(app);
         apiControlador.aplicarRutas();
-        AlertNotify.getInstancia().start();
+//        AlertNotify.getInstancia().start();
 
 
         while (true){
